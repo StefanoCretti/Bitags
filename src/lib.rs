a4061f491561ebc5011db8db53470f8a033908ed
Stefano Cretti <stefanocretti99@gmail.com>
2025-08-13 18:53:46 +0200

    bug: fixed incorrect bgzip file streaming
    Swapped GzDecoder with MultiGzDecoder, as the former interrupts the streaming of reads from a block gzipped file without raising any message or panicking.
    
    Minor:
    - Changed noodle version
    - Added print statement to log progress
    - Removed unnecessary BufReader before MultiGzDecoder
    - Removed pointless bstr crate

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,8 @@ mod tags;
 use barcodes::BarcodeBuilder;
 
 use flate2::Compression;
-use flate2::read::{GzDecoder, GzEncoder};
-use noodles_fastq as fastq;
+use flate2::read::{GzEncoder, MultiGzDecoder};
+use noodles::fastq;
 
 use std::{fs::File, io::BufReader, io::BufWriter};
 
@@ -15,16 +15,15 @@ pub fn barcode_reads(
     tags_file: std::path::PathBuf,
 ) {
     let mut reads_iter = File::open(raw_fastq)
+        .map(MultiGzDecoder::new)
         .map(BufReader::new)
-        .map(GzDecoder::new)
-        .map(BufReader::new)
-        .map(fastq::io::Reader::new)
+        .map(fastq::Reader::new)
         .unwrap();
 
     let output = File::create(new_fastq).unwrap();
     let gz_encoder = GzEncoder::new(output, Compression::default());
     let buf_writer = BufWriter::new(gz_encoder);
-    let mut writer = fastq::io::Writer::new(buf_writer);
+    let mut writer = fastq::Writer::new(buf_writer);
 
     let name_pos: usize = 1;
     let seq_pos: usize = 2;
@@ -32,7 +31,11 @@ pub fn barcode_reads(
 
     let tags_vec = tags::get_bitap_tags(&tags_file, name_pos, seq_pos, mism_pos);
     let mut builder = BarcodeBuilder::new(&tags_vec);
-    for res in reads_iter.records() {
+    for (i, res) in reads_iter.records().enumerate() {
+        if i % 1_000_000 == 0 {
+            println!("Processing read {}", i);
+        }
+
         let mut read = res.unwrap();
         let hits = builder.get_barcode(read.sequence());
 
